c0f93286d1eac2c3cce793dcd49561efb885017e
tiyo5151 <s1F102400224@iniad.org>
2024-12-03 23:54:39 +0900

    initial commit

---
+++ diary/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "diary"
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("page/create/", views.page_create, name="page_create"),
+]
